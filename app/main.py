f2f152421787d11aa3fcc8d4142bf980df5891f9
sobhani2525-collab <sobhani2525@gmail.com>
2026-09-04 10:12:07 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,8 +11,8 @@ app = FastAPI(
 )
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # tighten before production
+    CORSMiddleware,    
+    allow_origins=["https://incredible-paletas-837a8c.netlify.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
